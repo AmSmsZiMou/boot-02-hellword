70cce85905a1616145d2a21c717e5d2dd0132758
zimou <547593609@qq.com>
2023-01-13 23:30:24 +0800

    无

--- src/main/java/com/atguigu/boot/controller/HelloController.java
+++ src/main/java/com/atguigu/boot/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.atguigu.boot.controller;
 
 import com.atguigu.boot.bean.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
  * @create 2022--12--2022/12/16--15:50
  */
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
    @Autowired
     Person person;
 
 
-    @RequestMapping("/person")
-    public Person person(){
-        return person;
+    @GetMapping("")
+    public String hello(){
+        System.out.println("hello.world");
+
+        return "ok";
     }
 }
